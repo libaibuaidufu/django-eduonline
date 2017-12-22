512d0cefda26e043bfe2f69e66c607c9f70684ec
dfkai <983278926@qq.com>
2017-12-22 15:36:31 +0800

    add

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+import os,django
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoceshi.settings")
+    django.setup()
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
